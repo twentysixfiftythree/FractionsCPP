654773f32e288054ff0d3d7a5a2ec802203ba56f
Isaac Wood <20ipw@queensu.ca>
2025-10-03 09:07:41 -0400

    first commit

---
+++ Fractions/fraction20ipw.h
@@ -0,0 +1,41 @@
+//
+//  fraction20ipw.h
+//  Fractions
+//
+//  Created by Isaac W on 2025-10-03.
+//
+
+class FractionException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Fraction with 0 Denominator";
+    }
+};
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+public:
+    Fraction(int n = 0){
+        numerator = n;
+        denominator = 1;
+    }
+    Fraction(int n, int d){
+        if (d<0 and n < 0) {
+            numerator = abs(n);
+            denominator = abs(d);
+        }
+        else if (d<0 and n > 0){
+            numerator = -1*n;
+            denominator = abs(d);
+        }
+        else if (d == 0){
+            
+        }
+        numerator = n;
+        denominator = d;
+        
+    }
+    
+};
---
+++ Fractions/fraction_20ipw.cpp
@@ -0,0 +1,7 @@
+//
+//  fraction_20ipw.cpp
+//  Fractions
+//
+//  Created by Isaac W on 2025-10-03.
+//
+
