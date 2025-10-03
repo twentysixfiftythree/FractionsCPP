da2a3526955af49d381d4dc14aeaf0549b57a6c8
Isaac Wood <20ipw@queensu.ca>
2025-10-03 08:20:47 -0400

    Initial Commit

---
+++ Fractions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fractions
+//
+//  Created by Isaac W on 2025-10-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
